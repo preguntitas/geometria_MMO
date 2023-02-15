4a51ca57ca6877822037e5e18e52dc03899f56f1
Mariano Murcia Ortuño <m.murcia@gmail.com>
2023-02-15 23:36:21 +0100

    versión refactorizada

--- geometria/src/Circulo_MMO.java
+++ geometria/src/Circulo_MMO.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_MMO extends FiguraGeometrica_MMO {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_MMO(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
